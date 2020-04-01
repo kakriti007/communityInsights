5c98c856625bd5b743625bd88da3b20e18e56677
kakriti007 <61711277+kakriti007@users.noreply.github.com>
2020-03-31 17:30:58 -0700

    fsasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { NgbDateFRParserFormatter } from './ngb-date-fr-parser-formatter';
+import { NgbUTCStringAdapter } from './ngb-UTC-string-adapter';
+import {
+  NgbDropdownModule,
+  NgbModule,
+  NgbPopover,
+  NgbTabsetModule,
+  NgbDatepicker,
+  NgbDatepickerModule,
+  NgbDatepickerConfig,
+  NgbDateParserFormatter,
+  NgbDateAdapter
+} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TrendingComponent } from './trending/trending.component';
+import { VerbatimComponent } from './verbatim/verbatim.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ChartComponent } from './chart/chart.component';
+import { TreeMapComponent } from './tree-map/tree-map.component';
+import { TagInputModule } from 'ngx-chips';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TrendingComponent,
+    VerbatimComponent,
+    ChartComponent,
+    TreeMapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    TagInputModule,
   ],
-  providers: [],
+  exports: [],
+  providers: [
+    {
+      provide: NgbDateParserFormatter,
+      useClass: NgbDateFRParserFormatter
+    },
+    {
+      provide: NgbDateAdapter,
+      useClass: NgbUTCStringAdapter
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
